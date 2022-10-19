164d3d45395892714dec3490a33489039dcc0421
Darshana Jayarathna <jayarathnadarshana96@gmail.com>
2022-10-19 17:47:06 +0530

    Initial Commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+import { User } from './scheamas/user.scheama';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async getUserById(userId: string): Promise<User> {
+    return this.usersRepository.findOne({ userId });
+  }
+
+  async getUsers(): Promise<User[]> {
+    return this.usersRepository.find({});
+  }
+
+  async createUser(name: string, email: string): Promise<User> {
+    return this.usersRepository.create({
+      userId: uuidv4(),
+      name,
+      email,
+    });
+  }
+
+  async updateUser(userId: string, userUpdates: UpdateUserDto): Promise<User> {
+    return this.usersRepository.findOneAndUpdate({ userId }, userUpdates);
+  }
+}
